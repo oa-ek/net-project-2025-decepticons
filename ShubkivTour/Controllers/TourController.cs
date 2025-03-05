a9ffadc2fea4442ab9c3ab41c353b6dfa3af4f2f
DmytroDolzhenko <dolzenkodima14@gmail.com>
2025-03-05 18:07:53 +0200

    upd

--- ShubkivTour/Controllers/TourController.cs
+++ ShubkivTour/Controllers/TourController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using ShubkivTour.Models;
+using ShubkivTour.Models.Entity;
+using ShubkivTour.Models.DTO;
+using ShubkivTour.Repository.Interfaces;
 using System.Diagnostics;
 
 namespace ShubkivTour.Controllers
@@ -7,10 +10,12 @@ namespace ShubkivTour.Controllers
     public class TourController : Controller
     {
         private readonly ILogger<TourController> _logger;
+		private readonly ITour _tourRepository;
 
-        public TourController(ILogger<TourController> logger)
+		public TourController(ILogger<TourController> logger, ITour tourRepository)
         {
             _logger = logger;
+			_tourRepository = tourRepository;
         }
 
         public IActionResult TourManagement()
@@ -18,7 +23,31 @@ namespace ShubkivTour.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		[HttpPost]
+		public IActionResult GuideCreate(TourDTOCreate model)
+		{
+			if (ModelState.IsValid)
+			{
+				var tour = new Tour
+				{
+
+				};
+				_tourRepository.CreateTour(tour);
+				return RedirectToAction("TourManagement");
+			}
+			return View(model);
+			/*			if (ModelState.IsValid)
+						{
+							_guideRepository.CreateGuide(guide);
+							return RedirectToAction("GuideManagement");
+						}
+						return View(guide);*//*
+			_guideRepository.CreateGuide(guide);
+			return RedirectToAction("GuideManagement");*/
+
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
