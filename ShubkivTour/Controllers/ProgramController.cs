825cdb66a24a5845093c6aeb54fb040675ab6c1f
Andrii Zlotenko <workkanto@gmail.com>
2025-03-24 23:48:24 +0200

    morkva

---
+++ ShubkivTour/Controllers/ProgramController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShubkivTour.Controllers
+{
+	public class ProgramController : Controller
+	{
+		public IActionResult DayCreate()
+		{
+			return View();
+		}
+
+	}
+}
