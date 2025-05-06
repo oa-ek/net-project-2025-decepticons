4a06af753f563bb0c01972f5d09587dc99cf7952
Andrii Zlotenko <workkanto@gmail.com>
2025-05-06 16:54:19 +0300

    Change
    Vovka

---
+++ ShubkivTour/wwwroot/js/main.js
@@ -0,0 +1,21 @@
+﻿/*==SHOW MENU*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+/*== MENU SHOW ==*/
+/* Validate if constant exist */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exist */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
