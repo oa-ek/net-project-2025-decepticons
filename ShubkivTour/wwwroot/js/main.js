3871e91054b8537242ce58c90e0b58653627dbca
Andrii Zlotenko <workkanto@gmail.com>
2025-05-25 22:10:11 +0300

    NastiaOdesa
    upd

--- ShubkivTour/wwwroot/js/main.js
+++ ShubkivTour/wwwroot/js/main.js
@@ -68,13 +68,13 @@ window.addEventListener("scroll", scrollActive);
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 3000,
+    duration: 1500,
     delay: 400,
     // reset: true
 });
 
 sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`);
-sr.reveal(`.home__card`, { delay: 600, distance: '100px', interal: 100 });
+sr.reveal(`.home__card`, { delay: 300, distance: '100px', interal: 100 });
 sr.reveal(`.about__data, .join__image`, { origin: 'right' });
 sr.reveal(`.about__image, .join__data`, { origin: 'left' });
 sr.reveal(`.popular__card`, { interval: 200 });
